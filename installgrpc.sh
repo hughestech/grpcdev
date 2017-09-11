9e19a16561bf5bf7da677fb3f7641ea554facc7c
anton.hughes <kurrent93@gmail.com>
2017-09-12 09:16:05 +1200

    downgrade protoc

--- installgrpc.sh
+++ installgrpc.sh
@@ -7,10 +7,11 @@ make install
 
 #Install protoc-3
 # Make sure you grab the latest version
-wget https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-linux-x86_64.zip
+wget https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.3.0-linux-x86_64.zip
 
 # Unzip
-unzip protoc-3.4.0-linux-x86_64.zip -d protoc3
+
+unzip protoc-3.3.0-linux-x86_64.zip -d protoc3
 
 # Move protoc to /usr/local/bin/
 mv protoc3/bin/* /usr/local/bin/
