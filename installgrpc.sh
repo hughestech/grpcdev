b3c0b06fcfb8dbc86986fdf7810e5fc23ab86591
anton.hughes <kurrent93@gmail.com>
2017-09-12 13:46:05 +1200

    Try https://github.com/grpc/grpc/issues/9260#issuecomment-270744978

--- installgrpc.sh
+++ installgrpc.sh
@@ -5,17 +5,24 @@ git submodule update --init
 make
 make install
 
+cd third_party/protobuf
+./autogen.sh
+./configure
+make
+make check
+sudo make install
+
 #Install protoc-3
 # Make sure you grab the latest version - or not!
 protocVersion=3.3.0
-wget https://github.com/google/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-linux-x86_64.zip
+#wget https://github.com/google/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-linux-x86_64.zip
 
 # Unzip
 
-unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
+#unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
 
 # Move protoc to /usr/local/bin/
-mv protoc3/bin/* /usr/local/bin/
+#mv protoc3/bin/* /usr/local/bin/
 
 # Move protoc3/include to /usr/local/include/
-mv protoc3/include/* /usr/local/include/
+#mv protoc3/include/* /usr/local/include/
