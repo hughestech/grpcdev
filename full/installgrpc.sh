45e1edb1a6117429e6c78c8bae208fce11ab9626
anton.hughes <kurrent93@gmail.com>
2017-09-15 09:23:12 +1200

    Use script - proto 3.0.0

--- full/installgrpc.sh
+++ full/installgrpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-git clone -b v1.6.x --single-branch https://github.com/grpc/grpc
+git clone -b ${GRPC_RELEASE_TAG} --single-branch https://github.com/grpc/grpc /var/local/git/grpc
 cd grpc
 git submodule update --init
 make
@@ -14,15 +14,14 @@ make install
 
 #Install protoc-3
 # Make sure you grab the latest version - or not!
-protocVersion=3.3.0
+protocVersion=3.0.0
 #wget https://github.com/google/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-linux-x86_64.zip
 
 # Unzip
-
-#unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
+unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
 
 # Move protoc to /usr/local/bin/
-#mv protoc3/bin/* /usr/local/bin/
+mv protoc3/bin/* /usr/local/bin/
 
 # Move protoc3/include to /usr/local/include/
-#mv protoc3/include/* /usr/local/include/
+mv protoc3/include/* /usr/local/include/
