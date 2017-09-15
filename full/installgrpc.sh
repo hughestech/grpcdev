32e74632ffa5245e5e3e39230e1c02e1dba3d951
anton.hughes <kurrent93@gmail.com>
2017-09-15 14:01:50 +1200

    dont copy 64bit over 32bit!

--- full/installgrpc.sh
+++ full/installgrpc.sh
@@ -7,21 +7,24 @@ make install
 
 cd third_party/protobuf
 ./autogen.sh
-./configure CC='winegcc -m32' CXX='wineg++ -m32'
+./configure
 make
 make check
 make install
 
+#./configure CC='winegcc -m32' CXX='wineg++ -m32'
+
+
 #Install protoc-3
 # Make sure you grab the latest version - or not!
-protocVersion=3.0.0
-wget https://github.com/google/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-linux-x86_64.zip
+#protocVersion=3.0.0
+#wget https://github.com/google/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-linux-x86_64.zip
 
 # Unzip
-unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
+#unzip protoc-$protocVersion-linux-x86_64.zip -d protoc3
 
 # Move protoc to /usr/local/bin/
-mv protoc3/bin/* /usr/local/bin/
+#mv protoc3/bin/* /usr/local/bin/
 
 # Move protoc3/include to /usr/local/include/
-mv protoc3/include/* /usr/local/include/
+#mv protoc3/include/* /usr/local/include/
